1a4c0b27814af874224038b13627fe8940737e8b
Daniel Alves <daniel.alves@skytef.intranet>
2021-07-13 10:32:07 -0300

    first

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,31 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from './Button'
+import '../styles/sidebar.scss';
+interface GenreItemsList {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+interface GenreProps {
+  genres: Array<GenreItemsList>
+  selectedGenreId: number
+  onHandleClickItemSideBar(id: number): void
+}
+
+export function SideBar({ genres, selectedGenreId, onHandleClickItemSideBar }: GenreProps) {
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => onHandleClickItemSideBar(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  )
 }
